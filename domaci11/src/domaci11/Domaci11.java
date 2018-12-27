18f60b1319b347afc42529f4577182cbbb53a8bb
Andrija Erjavec <nikolaj1popovic@gmail.com>
2018-12-27 14:19:50 +0100

    Add files via upload

---
+++ domaci11/src/domaci11/Domaci11.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domaci11;
+
+/**
+ *
+ * @author student
+ */
+public class Domaci11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+                System.out.println("Hello World" + c );
+
+    }
+    
+}
